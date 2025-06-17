14d0f2bd2ee6a328506cb1e6477cfca9f3a842b7
fauziamg <fauziamg@fauziamgs-MacBook-Air.local>
2025-06-17 10:49:32 +0700

    first commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path'
+
+const app = express();
+const port = 3000;
+app.use(express.static(path.join(__dirname)))
+
+app.get('/', (req, res) => {
+   res.sendFile(path.join(__dirname, 'index.html'))
+//   res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
