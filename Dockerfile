ff49c351cb4c494e346f78b2ddada96de57011d8
fauziamg <fauziamg@fauziamgs-MacBook-Air.local>
2025-06-19 14:45:27 +0700

    update react

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,10 @@ COPY package*.json ./
 
 RUN npm install
 
+RUN npm build
+
 COPY . .
 
 EXPOSE 3000
 
-CMD ["npm", "run" ,"dev"]
+CMD ["node", "dist/app"]
--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import path from 'path'
 
 const app = express();
 const port = 3000;
-app.use(express.static(path.join(__dirname)))
+app.use(express.static(path.join(__dirname,)))
 
 app.get('/', (req, res) => {
    res.sendFile(path.join(__dirname, 'index.html'))
